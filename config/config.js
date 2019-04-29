c164a7ec574ddfda6b069c55cc859f3afe46ae21
Nick Dean <klick5000@gmail.com>
2019-04-29 01:28:32 -0600

    updating travis config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
+if (
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'travis'
+) {
   // Production state - return prod keys
   module.exports = require('./prodKeys');
 } else {
